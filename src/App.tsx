6fdce3252431e4798c089331359ad33f831e6e00
Lukas Petry <lukemt@rennwurst.de>
2021-10-24 02:26:59 +0200

    📝-3 implement Notes-Array and Notes-Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,57 @@
 import { useState } from "react";
 import ContentEditable from "./ContentEditable";
+import Notes from "./Note";
+import { Note } from "./types";
+
+const initialNotes: Note[] = [
+  {
+    _id: "1",
+    text: "Hello World",
+    children: ["2", "3", "5"],
+  },
+  {
+    _id: "2",
+    text: "This is a note",
+    children: [],
+  },
+  {
+    _id: "3",
+    text: "This is another note",
+    children: ["4"],
+  },
+  {
+    _id: "4",
+    text: "This is another note",
+    children: [],
+  },
+  {
+    _id: "5",
+    text: "This is another note what???",
+    children: [],
+  },
+];
 
 function App() {
-  const [value, setValue] = useState("Hello World!");
+  const [notes, setNotes] = useState(initialNotes);
+
+  function updateNote(id: string, text: string) {
+    setNotes(
+      notes.map((note) => {
+        if (note._id === id) {
+          return { ...note, text };
+        }
+        return note;
+      })
+    );
+  }
+
   return (
     <>
       <header className="fixed top-0 inset-x-0 p-5 bg-gradient-to-br from-white to-blue-50 shadow-md">
         <h1>Hello World</h1>
       </header>
       <main className="max-w-sm mx-auto my-20">
-        <ul>
-          <li>
-            <ContentEditable
-              defaultValue={value}
-              onNewValue={setValue}
-              className="px-5 py-3 m-5 rounded-xl bg-gradient-to-br from-white to-blue-50 shadow-lg"
-            />
-            <ul className="pl-20">
-              <li>
-                <ContentEditable
-                  defaultValue={value}
-                  onNewValue={setValue}
-                  className="px-5 py-3 m-5 rounded-xl bg-gradient-to-br from-white to-blue-50 shadow-lg"
-                />
-              </li>
-              <li>
-                <ContentEditable
-                  defaultValue={value}
-                  onNewValue={setValue}
-                  className="px-5 py-3 m-5 rounded-xl bg-gradient-to-br from-white to-blue-50 shadow-lg"
-                />
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <Notes notes={notes} onUpdate={updateNote} id={"1"} />
       </main>
       <footer className="fixed bottom-0 inset-x-0 p-5  bg-gradient-to-br from-white to-blue-50 shadow-xl">
         foo
