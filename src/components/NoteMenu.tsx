8a246612e13010f1f271af28a4de7ede3d6288e6
Lukas Petry <lukemt@rennwurst.de>
2021-11-12 23:47:35 +0100

    Change toggle isPage icon

--- src/components/NoteMenu.tsx
+++ src/components/NoteMenu.tsx
@@ -10,7 +10,7 @@ export default function NoteMenu({ isPage, onToggleIsPage }: NoteMenuProps) {
   return (
     <StyledButton
       onClick={onToggleIsPage}
-      className="p-3 opacity-0 group-hover:opacity-100 text-gray-400 dark:text-gray-600 hover:text-gray-900 dark:hover:text-gray-50 hover:scale-125 transition"
+      className="p-3 opacity-0 group-hover:opacity-100 text-gray-400 dark:text-gray-600 hover:text-gray-700 dark:hover:text-gray-50 hover:scale-125 transition"
     >
       {isPage ? (
         <svg
@@ -21,7 +21,7 @@ export default function NoteMenu({ isPage, onToggleIsPage }: NoteMenuProps) {
         >
           <path
             fillRule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z"
             clipRule="evenodd"
           />
         </svg>
@@ -34,7 +34,7 @@ export default function NoteMenu({ isPage, onToggleIsPage }: NoteMenuProps) {
         >
           <path
             fillRule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
             clipRule="evenodd"
           />
         </svg>
